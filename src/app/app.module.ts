5dda4ef3cfa0f0c417c5bd87a414ef08b9b91350
Tiago Neves Leao Costa Lino <tiagobrasil18@gmail.com>
2023-01-31 22:51:52 -0300

    Criando formulário, validações, post do form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {LOCALE_ID} from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +31,18 @@ import { DialogComponent } from './dialog/dialog.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators  } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
+
+  freshnessList = ["Novo", "Segunda Mão", "Remodelado"];
+  productForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder, private api : ApiService, private dialogRef : MatDialogRef<DialogComponent>) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName : ['', Validators.required],
+      category : ['', Validators.required],
+      freshness : ['', Validators.required],
+      price : ['', Validators.required],
+      comment : ['', Validators.required],
+      date : ['', Validators.required]
+    })
+  }
+  addProduct(){
+    if(this.productForm.valid){
+      this.api.postProduct(this.productForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("Produto adicionado com sucesso!")
+          this.productForm.reset();
+          this.dialogRef.close('save');
+        },
+        error:()=>{
+          alert("Erro ao adicionar o produto!")
+        }
+      })
+
+    }
+  }
 
 }
